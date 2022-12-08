2957ed26bfe478995d479234894f57d76e80497d
gxy1113 <gxy981113@gmail.com>
2022-12-08 16:39:12 -0500

    init

--- log_timer.sh
+++ log_timer.sh
@@ -1,6 +1,8 @@
 APPNAME=$1
+sleep 300
 for i in {1..12}
 do
-    sleep 1
-    php combine.php "reports-$i" $APPNAME
+    sleep 7200
+    #php combine.php "reports-$i" $APPNAME
+    cp -TRv coverages/ coverages-$i/
 done
