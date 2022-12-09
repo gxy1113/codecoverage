a71d1b5a65cb609686d9027d3976e2d1e9d31fa8
gxy1113 <gxy981113@gmail.com>
2022-12-09 11:55:15 -0500

    test

--- log_timer.sh
+++ log_timer.sh
@@ -4,6 +4,6 @@ for i in `seq 1 12`;
 do
     sleep 7200
     echo "take snapshot of coverages-$i"
-    php combine.php "reports-$i" $APPNAME
+    screen -dmS test-$i php combine.php "reports-$i" $APPNAME
     #cp -TR coverages/ coverages-$i/
 done
