2ccddad56672c70b024a0f8a7ef30a4dc4e9f54f
gxy1113 <gxy981113@gmail.com>
2022-12-09 10:57:10 -0500

    test

--- log_timer.sh
+++ log_timer.sh
@@ -1,8 +1,9 @@
 APPNAME=$1
 sleep 300
-for i in {1..12}
+for i in `seq 1 12`;
 do
     sleep 7200
-    #php combine.php "reports-$i" $APPNAME
-    cp -TRv coverages/ coverages-$i/
+    echo "take snapshot of coverages-$i"
+    php combine.php "reports-$i" $APPNAME
+    #cp -TR coverages/ coverages-$i/
 done
