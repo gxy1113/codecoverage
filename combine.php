8aacc77acf5f398678ba43d941baabd966108f7e
gxy1113 <gxy981113@gmail.com>
2022-12-28 14:18:32 -0500

    init

--- combine.php
+++ combine.php
@@ -1,12 +1,17 @@
 <?php
     include_once("vendor/autoload.php");
-    $report_name = $argv[1];
+    $time_interval = 3600;
+    $iterations = $argv[1];
+    $report_name = "reports_$iterations";
+    $iterations = (int) $iterations;
+    $time_limit = $iterations * $time_interval;
     $appname = $argv[2];
+    $start_time = (int) $argv[3];
     $exist = file_exists("cookies/a_$appname.json"); #using whether file exist to enable the filtering mode.
     if($exist){
         $rough_data = file_get_contents("cookies/a_$appname.json");
         $cookies = json_decode($rough_data);
-    }
+    }   
     $coverages = glob("coverages/*.json");
     #increase the memory in multiples of 128M in case of memory error
     ini_set('memory_limit', '12800M');
@@ -15,7 +20,7 @@
     $count = count($coverages);
     $i = 0;
 
-    $final_coverage->filter()->addDirectoryToWhitelist("/var/www/$appname/");
+    $final_coverage->filter()->addDirectoryToWhitelist("/home/vmuser/$appname/");
     echo "start iterating" . PHP_EOL;
     foreach ($coverages as $coverage_file)
     {   
@@ -34,6 +39,15 @@
                 continue;
             }
         }
+        $file_arr = explode("-", $coverage_file);
+        $time_arr = explode(".", $file_arr[2]);
+        $time_stamp = $time_arr[0];
+        $time_stamp = (int) $time_stamp;
+        $related_time = $time_stamp - $start_time;
+        if($related_time > $time_limit){
+            echo "larger than the time limit, skip" . PHP_EOL;
+            continue;
+        }
         echo "Processing coverage ($i/$count) from $coverage_file". PHP_EOL;
         $codecoverageData = json_decode(file_get_contents($coverage_file), JSON_OBJECT_AS_ARRAY);
         if($codecoverageData == NULL){
@@ -42,7 +56,7 @@
         }
         $test_name = str_ireplace(basename($coverage_file,".json"),"coverage-", "");
         $final_coverage->append($codecoverageData, $test_name);
-    }   
+    }
 
     echo "Generating final report..." . PHP_EOL;
     $report = new \SebastianBergmann\CodeCoverage\Report\Html\Facade;
