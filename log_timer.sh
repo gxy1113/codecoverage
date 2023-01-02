a5b3ba90753628f8f19f1cc4f161fb96d74a11eb
gxy1113 <gxy981113@gmail.com>
2023-01-02 15:28:25 -0500

    init

--- log_timer.sh
+++ log_timer.sh
@@ -1,9 +1,10 @@
 APPNAME=$1
-sleep 300
-for i in `seq 1 12`;
+START_TIME=1672452949
+sleep 10
+for i in `seq 1 24`;
 do
-    sleep 7200
     echo "take snapshot of coverages-$i"
-    screen -dmS test-$i php combine.php "reports-$i" $APPNAME
+    screen -dmS test-$i php combine.php $i $APPNAME $START_TIME;
+    sleep 1800
     #cp -TR coverages/ coverages-$i/
 done
